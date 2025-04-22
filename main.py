720aa270e03edb17e41cb518873abcc992f9779f
Win Cutrer <91359232+wincutrer@users.noreply.github.com>
2025-04-22 16:17:10 -0400

    A: initial pygame setup with black screen and gitignore

---
+++ main.py
@@ -0,0 +1,26 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+            screen.fill((0, 0, 0))  # Fill with black RGB
+            pygame.display.flip()  # Update the display
+
+
+if __name__ == "__main__":
+    main()
